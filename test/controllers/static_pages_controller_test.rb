1e18f5290496d81079052953475bc7075288f1af
Chloe Wolf <chloe.wolf@colorado.edu>
2015-03-02 00:35:16 +0000

    tis working

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,16 +5,12 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-<<<<<<< HEAD
     assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
-=======
->>>>>>> static-pages
   end
 
   test "should get help" do
     get :help
     assert_response :success
-<<<<<<< HEAD
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
@@ -22,7 +18,5 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :about
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
-=======
->>>>>>> static-pages
   end
 end
