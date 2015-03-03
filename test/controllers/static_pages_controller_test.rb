f63c3f1775e60208a09376df8ed0e4eac26c1127
Chloe Wolf <chloe.wolf@colorado.edu>
2015-03-03 01:32:21 +0000

    ignored c9 files

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,12 +1,6 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-<<<<<<< HEAD
-  test "should get home" do
-    get :home
-    assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
-=======
 
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
@@ -16,26 +10,18 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :home
     assert_response :success
     assert_select "title", "Home | #{@base_title}"
->>>>>>> static-pages-exercises
   end
 
   test "should get help" do
     get :help
     assert_response :success
-<<<<<<< HEAD
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
-=======
     assert_select "title", "Help | #{@base_title}"
->>>>>>> static-pages-exercises
   end
 
   test "should get about" do
     get :about
     assert_response :success
-<<<<<<< HEAD
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
-=======
     assert_select "title", "About | #{@base_title}"
->>>>>>> static-pages-exercises
-  end
-end
+  end  
+    
+end     
